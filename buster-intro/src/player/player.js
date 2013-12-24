bcb1e5a73ab7bc31b7df6e475388eaed7101ad64
reshet <reshet.ukr@gmail.com>
2013-12-24 15:42:58 +0200

    QUnit test example

---
+++ buster-intro/src/player/player.js
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: reshet
+ * Date: 12/24/13
+ * Time: 3:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function play(anket){
+    return anket;
+}
---
+++ buster-intro/test/player/playerTest.js
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: reshet
+ * Date: 12/24/13
+ * Time: 3:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+test('play()', function () {
+
+    //equals(trim(''), '', 'Пустая строка');
+    //ok(trim('   ') === '', 'Строка из пробельных символов');
+    //same(trim(), '', 'Без параметра');
+    equal(play("myanket"),"myanket","Пример теста на равенство");
+});
